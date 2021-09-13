554a6c6f0348bb118fd2e05f31a3326d73b974b8
UssuyNeumann <luis.eduardo.un@hotmail.com>
2021-09-12 23:29:12 -0300

    Adicionando form com  post

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP - Competição</title>
+</head>
+<body>
+<p>Formulário para Inscrição de Competidores</p>
+<form action="script.php" method="POST">
+    <label for="nome">Nome: </label> <br> 
+    <input type="text" name="nome" > <br><br> 
+    <label for="idade">Idade: </label> <br> 
+    <input type="text" name="idade" > <br><br> 
+    <input type="submit" value="ENVIAR">
+</form>
 
-$categorias = [];
-$categorias[] = "infantil";
-$categorias[] = "juvenil";
-$categorias[] = "adulto";
-
-print_r($categorias);
-
-?>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,49 @@
+<?php
+
+//variavel/array 
+$categorias[];
+
+$categorias[] = "Infantil";
+$categorias[] = "Juvenil";
+$categorias[] = "Adulto";
+$categorias[] = "Sênior";
+
+//metodos de recebimento de dados
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+// condições e laços de repetição 
+if($idade >= 6 && $idade <= 12)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == "Infantil")
+            echo "O nadador ".$nome. " compete na categoria ".$categorias[$i]"."; 
+    } 
+}
+else if($idade >= 13 && $idade <= 17)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == "Juvenil")
+            echo "O nadador ".$nome. " compete na categoria ".$categorias[$i]"."; 
+    } 
+}
+else if($idade >= 18 && $idade <= 60)
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == "Adulto")
+            echo "O nadador ".$nome. " compete na categoria ".$categorias[$i]"."; 
+    } 
+}
+else if($idade >= 60 )
+{
+    for($i = 0; $i <= count($categorias); $i++)
+    {
+        if($categorias[$i] == "Sênior")
+            echo "O nadador ".$nome. " compete na categoria ".$categorias[$i]"."; 
+    } 
+}
+else {}
+?>
