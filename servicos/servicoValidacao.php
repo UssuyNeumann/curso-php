95bec0982bfba37f71231605ca74e7b14dd0f4bf
UssuyNeumann <luis.eduardo.un@hotmail.com>
2021-09-13 22:33:43 -0300

    Crindo funções

--- servicos/servicoValidacao.php
+++ servicos/servicoValidacao.php
@@ -5,9 +5,9 @@ function validaNome(string $nome)
     if(empty($nome))
     {
         $_SESSION['mensagem-de-erro'] = 'Todos os campos devem ser preenchidos!';
-        header('Location: index.php');
-        return;
+        return false;
     }
+    return true;
 }
 
 function validaIdade(string $idade)
@@ -15,9 +15,9 @@ function validaIdade(string $idade)
     if(empty($idade))
     {
         $_SESSION['mensagem-de-erro'] = 'Todos os campos devem ser preenchidos!';
-        header('Location: index.php');
-        return;
+        return false;
     }
+    return true;
 }
 
 ?>
